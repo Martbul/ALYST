0b07e7f0363c3808a3f5218de066886a21c6ff15
Martbul <martbul01@gmail.com>
2023-12-28 17:28:36 +0200

    343
    4343

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -433,6 +433,7 @@ export default function About() {
            </div>
          </div>
        </section>
+
      </>
    );
 }
--- client/src/paths.js
+++ client/src/paths.js
@@ -19,4 +19,5 @@ const Path = {
   DeliverLogin: "/deliverLogin",
 };
 
+
 export default Path;
